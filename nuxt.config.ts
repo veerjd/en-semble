ee02e7b308a28ace4234d4a694133b4e9858028c
veerjd <veerjd@gmail.com>
2024-12-07 21:19:18 -0500

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./app/components/ui"
+     */
+    componentDir: './app/components/ui'
+  }
+})
+// 
